b4557a2ddfa830e96981b0422b4cfea63034e2d6
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-06-19 21:39:23 +0530

    react query for product page

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
 const Product = () => {
-  return <div>Product</div>;
+  const { productId } = useParams();
+
+  const fetchProduct = async () => {
+    const response = await fetch(`https://dummyjson.com/products/${productId}`);
+
+    const data = await response.json();
+
+    return data;
+  };
+
+  const {
+    isLoading,
+    error,
+    data: product,
+  } = useQuery({
+    queryKey: ["product", productId],
+    queryFn: fetchProduct,
+  });
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (error) {
+    return <h3>Error: {error}</h3>;
+  }
+  return (
+    <div>
+      Product: {product.title}
+      <img src={product.images[0]} alt="" />
+    </div>
+  );
 };
 
 export default Product;
