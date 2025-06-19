86a78cd040ce3c4b89942521e1eeda3905ed572d
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-06-20 00:19:44 +0530

    mutations and useMutation hook

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const Product = () => {
   const { productId } = useParams();
@@ -14,23 +15,73 @@ const Product = () => {
 
   const {
     isLoading,
-    error,
+    error: queryError,
     data: product,
   } = useQuery({
     queryKey: ["product", productId],
     queryFn: fetchProduct,
   });
 
+  //! Mutations
+
+  //! Mutation is used when we need to update the state
+  const {
+    mutate,
+    isPending,
+    isError,
+    error: mutationError,
+  } = useMutation({
+    mutationFn: (newProduct) => {
+      return axios.put(
+        `https://dummyjson.com/produuucts/${productId}`,
+        newProduct
+      );
+    },
+  });
+
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
-  if (error) {
-    return <h3>Error: {error}</h3>;
+  if (queryError) {
+    return (
+      <h3>
+        Error:{" "}
+        {queryError?.response?.data?.message ||
+          queryError?.message ||
+          "Something went wrong"}
+      </h3>
+    );
+  }
+
+  //! mutation state
+  if (isPending) {
+    return <h2>Updating title</h2>;
+  }
+
+  if (isError) {
+    return (
+      <h3>
+        Error:{" "}
+        {mutationError?.response?.data?.message ||
+          mutationError?.message ||
+          "Something went wrong"}
+      </h3>
+    );
   }
   return (
     <div>
       Product: {product.title}
       <img src={product.images[0]} alt="" />
+      <button
+        style={{ backgroundColor: "skyblue", color: "darkgray" }}
+        onClick={() => {
+          mutate({
+            title: "Updated title",
+          });
+        }}
+      >
+        Update Button
+      </button>
     </div>
   );
 };
